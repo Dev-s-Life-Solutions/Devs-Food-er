129c651d6fc1935da81443e427ef9527321f5c99
ANTHONY <anthonymhs0320@gmail.com>
2024-06-06 09:48:37 -0500

    nuevo archivo html,css y js

---
+++ js/recipe.js
@@ -0,0 +1,157 @@
+function Recipe(name, img, ingredients, steps) {
+    this.name = name;
+    this.img = img;
+    this.ingredients = ingredients;
+    this.steps = steps;
+}
+
+const recipe1 = new.Recipe(
+
+)
+const recipe2 = new.Recipe(
+
+)
+const recipe3 = new.Recipe(
+
+)
+const recipe4 = new.Recipe(
+
+)
+const recipe5 = new.Recipe(
+
+)
+const recipe6 = new.Recipe(
+
+)
+const recipe7 = new.Recipe(
+
+)
+const recipe8 = new.Recipe(
+
+)
+const recipe9 = new.Recipe(
+
+)
+const recipe10 = new.Recipe(
+
+)
+const recipe11 = new.Recipe(
+
+)
+const recipe12 = new.Recipe(
+
+)
+const recipe13 = new.Recipe(
+
+)
+const recipe14 = new.Recipe(
+
+)
+const recipe15 = new.Recipe(
+
+)
+const recipe16 = new.Recipe(
+
+)
+const recipe17 = new.Recipe(
+
+)
+const recipe18 = new.Recipe(
+
+)
+const recipe19 = new.Recipe(
+
+)
+const recipe20 = new.Recipe(
+
+)
+const recipe21 = new.Recipe(
+
+)
+const recipe22 = new.Recipe(
+
+)
+const recipe23 = new.Recipe(
+
+)
+const recipe24 = new.Recipe(
+
+)
+const recipe25 = new.Recipe(
+
+)
+const recipe26 = new.Recipe(
+
+)
+const recipe27 = new.Recipe(
+
+)
+const recipe28 = new.Recipe(
+
+)
+const recipe29 = new.Recipe(
+
+)
+const recipe30 = new.Recipe(
+
+)
+const recipe31 = new.Recipe(
+
+)
+const recipe32 = new.Recipe(
+
+)
+const recipe33 = new.Recipe(
+
+)
+const recipe34 = new.Recipe(
+
+)
+const recipe35 = new.Recipe(
+
+)
+const recipe36 = new.Recipe(
+
+)
+const recipe37 = new.Recipe(
+
+)
+const recipe38 = new.Recipe(
+
+)
+const recipe39 = new.Recipe(
+
+)
+const recipe40 = new.Recipe(
+
+)
+const recipe41 = new.Recipe(
+
+)
+const recipe42 = new.Recipe(
+
+)
+const recipe43 = new.Recipe(
+
+)
+const recipe44 = new.Recipe(
+
+)
+const recipe45 = new.Recipe(
+
+)
+const recipe46 = new.Recipe(
+
+)
+const recipe47 = new.Recipe(
+
+)
+const recipe48 = new.Recipe(
+
+)
+const recipe49 = new.Recipe(
+
+)
+const recipe50 = new.Recipe(
+
+)
