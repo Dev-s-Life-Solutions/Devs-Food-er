8c48d1723d6efc62f938137cafa20b42f8f09194
angelalonsopazruiz <angel.paz.consultas@gmail.com>
2024-06-13 00:42:15 -0500

    se renderizan cards y recetas totales

--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
-// const show_result_button = document.getElementById('show_result_btn');
-// show_result_button.hidden = true;
-const revert_selection_button = document.getElementById('revert_selection_btn');
-revert_selection_button.hidden = true;
+
 const select_ingredients_button = document.getElementById('select_ingredients_btn');
 const selected_recipe_container = document.getElementById('selected_recipe_container');
 
@@ -19,26 +16,25 @@ function filterRecipesByIngredients(selectedIngredients) {
     );
 }
 
-if (select_ingredients_button){
-select_ingredients_button.addEventListener('click', function () {
-    const selectedIngredients = getSelectedIngredients();
-    if (selectedIngredients.length > 0) {
-        filteredRecipes = filterRecipesByIngredients(selectedIngredients);
-        console.log('Filtered Recipes:', filteredRecipes);
+if (select_ingredients_button) {
+    select_ingredients_button.addEventListener('click', function () {
+        const selectedIngredients = getSelectedIngredients();
+        if (selectedIngredients.length > 0) {
+            filteredRecipes = filterRecipesByIngredients(selectedIngredients);
+            console.log('Filtered Recipes:', filteredRecipes);
 
-        // show_result_button.hidden = false;
-        revert_selection_button.hidden = false;
-        select_ingredients_button.hidden = true;
-        localStorage.setItem('filteredRecipes', JSON.stringify(filteredRecipes));
-        window.location.href = 'selected_recipes.html';
-    } else {
-        alert('Selecciona al menos un ingrediente.');
-    }
-});
-renderIngredients();
+
+            select_ingredients_button.hidden = true;
+            localStorage.setItem('filteredRecipes', JSON.stringify(filteredRecipes));
+            window.location.href = 'selected_recipes.html';
+        } else {
+            alert('Selecciona al menos un ingrediente.');
+        }
+    });
+    renderIngredients();
 }
 
-if (selected_recipe_container){
+if (selected_recipe_container) {
     const ls_filtered_recipes = JSON.parse(localStorage.getItem('filteredRecipes'));
     console.log(ls_filtered_recipes);
     ls_filtered_recipes.forEach(recipe => {
@@ -46,8 +42,64 @@ if (selected_recipe_container){
         console.log(cardRecipe);
         selected_recipe_container.appendChild(cardRecipe);
     });
-
 }
+
+const full_recipe_container = document.getElementById('full_recipe_container');
+if (full_recipe_container) {
+    const recipe = JSON.parse(localStorage.getItem('selectedRecipe'));
+    if (recipe) {
+        const recipeName = document.createElement('h2');
+        const recipeServings = document.createElement('p');
+        const recipeDifficulty = document.createElement('p');
+        const recipeImg = document.createElement('img');
+        const recipeListIngrt = document.createElement('ul');
+        const recipeListSteps = document.createElement('ol');
+        const recipeCost = document.createElement('p');
+
+        recipeName.textContent = recipe.name;
+        recipeServings.textContent = recipe.servings;
+        recipeDifficulty.textContent = recipe.difficulty;
+        recipeImg.src = recipe.img;
+        recipeImg.alt = recipe.name;
+        recipeListIngrt.textContent = 'Ingredientes :';
+        recipeCost.textContent = recipe.cost;
+        recipeListSteps.textContent = 'Pasos a Seguir :';
+
+        full_recipe_container.appendChild(recipeName);
+        full_recipe_container.appendChild(recipeServings);
+        full_recipe_container.appendChild(recipeDifficulty);
+        full_recipe_container.appendChild(recipeImg);
+        full_recipe_container.appendChild(recipeListIngrt);
+        recipe.ingredientsDetailed.forEach(ingredientsDetailed => {
+            let item = document.createElement('li');
+            item.textContent = ingredientsDetailed;
+            full_recipe_container.appendChild(item);
+        });
+        full_recipe_container.appendChild(recipeListSteps);
+
+        recipe.steps.forEach(steps => {
+            let item = document.createElement('li');
+            item.textContent = steps;
+            full_recipe_container.appendChild(item);
+        });
+
+        recipeListSteps.appendChild(recipeSteps);
+        full_recipe_container.appendChild(recipeCost);
+    } else {
+        container.textContent = 'No se encontró la receta seleccionada.';
+    }
+}
+
+const container_all_recipes = document.getElementById('container_all_recipes');
+if (container_all_recipes) {
+
+        recipes.forEach(recipe => {
+            const card = renderRecipe(recipe);
+            container_all_recipes.appendChild(card);
+        });
+}
+
+
 
 
 
