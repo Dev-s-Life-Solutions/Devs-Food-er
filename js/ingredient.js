456376419ca73477be59bc4ce26a5da7f0bb86bd
angelalonsopazruiz <angel.paz.consultas@gmail.com>
2024-06-13 22:17:18 -0500

    Se aplica estructura AppState

--- js/ingredient.js
+++ js/ingredient.js
@@ -4,40 +4,19 @@ function Ingredient(id, name, img) {
     this.img = img;
 }
 
-const ingredients = [
-    new Ingredient('atun', 'Atún', '/img/atun.jpg'),
-    new Ingredient('cebolla', 'Cebolla', '..\img\cebolla.jpg'),
-    new Ingredient('pan', 'Pan', '..\img\pan.jpg'),
-    new Ingredient('leche', 'Leche', '..\img\leche.jpg'),
-    new Ingredient('papa', 'Papa', '..\img\papa.jpg'),
-    new Ingredient('queso_parmesano', 'Queso Parmesano', '..\img\queso_parmesano.jpg'),
-    new Ingredient('comino', 'Comino', '..\img\comino.jpg'),
-    new Ingredient('oregano', 'Orégano', '..\img\oregano.jpg'),
-    new Ingredient('pimienta', 'Pimienta', '..\img\pimienta.jpg'),
-    new Ingredient('aceituna', 'Aceituna', '..\img\aceituna.jpg'),
-    new Ingredient('huevo', 'Huevo', '..\img\huevo.jpg'),
-    new Ingredient('aceite', 'Aceite', '..\img\aceite.jpg'),
-    new Ingredient('cerdo', 'Cerdo', '..\img\cerdo.jpg'),
-    new Ingredient('pimenton', 'Pimenton', '..\img\pimenton.jpg'),
-
-]
-
-function renderIngredients() {
-    const container = document.getElementById('ingredients_container');
-    container.innerHTML = '';
-    ingredients.forEach(ingredient => {
-        const ingredientElement = document.createElement('div');
-        ingredientElement.classList.add('ingredient');
-        ingredientElement.innerHTML = `
-        <input type="checkbox" id="${ingredient.id}" name="ingredient" value="${ingredient.id}">
-        <label for="${ingredient.id}">
-            <img src="${ingredient.img}" alt="${ingredient.name}">
-            <span>${ingredient.name}</span>
+Ingredient.prototype.render = function () {
+    const ingredientElement = document.createElement('div');
+    ingredientElement.classList.add('ingredient');
+    ingredientElement.innerHTML = `
+        <input type="checkbox" id="${this.id}" name="ingredient" value="${this.id}">
+        <label for="${this.id}">
+            <img src="${this.img}" alt="${this.name}">
+            <span>${this.name}</span>
         </label>
     `;
-        container.appendChild(ingredientElement);
-    });
+    return ingredientElement;
 }
+
 
 
 // atun cebolla pan leche papa queso_parmesano comino oregano pimienta aceituna huevo aceite cerdo pimenton aji_molido aji pollo arveja choclo tomate perejil culantro margarina harina nuez_moscada alcachofa pulpa_cangrejo mayonesa concha limon 
